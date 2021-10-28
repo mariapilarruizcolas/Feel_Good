fd67cff0d252cdfbda16886ea515d5374a1b2614
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2021-10-28 13:06:07 +0200

    Nav Barr responsive done

--- src/components/NavBarr.jsx
+++ src/components/NavBarr.jsx
@@ -1,38 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBarr.css';
-
-/*import BienManger from './BienManger';
-import BienBouger from './BienBouger';
-import BienDansSaTete from './BienDansSaTete';
-import Home from './components/index.jsx';
-className={style.body}*/
-
 function NavBarr() {
   return (
-    <div className="NavBarr">
-      <ul className="Header">
-        <li>
-          <NavLink activeClassName="active" exact to="/">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/BienBouger">
-            Bien Bouger
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/BienManger">
-            Bien Manger
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/BienDansSaTete">
-            Bien dans sa tête
-          </NavLink>
-        </li>
-      </ul>
+    <div>
+      <div className="NavBarrDescktop">
+        <ul className="Header">
+          <li>
+            <NavLink activeClassName="active" to="/BienBouger">
+              Bien Bouger
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/BienManger">
+              Bien Manger
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/BienDansSaTete">
+              Bien dans sa tête
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="NavBarrMobile">
+        <ul className="Top">
+          <li>
+            <NavLink activeClassName="active" to="/BienBouger">
+              <a href="./BienBouger" className="logo">
+                <img className="logo" src="../img/bienBouger.png" alt="Bien Bouger" />
+              </a>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/BienManger">
+              <a href="./BienManger.jsx" className="logo">
+                <img className="logo" src="../img/bienManger.png" alt="Bien Manger" />
+              </a>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/BienDansSaTete">
+              <a href="./BienDansSaTete.jsx" className="logo">
+                <img className="logo" src="../img/bienDansSaTete.png" alt="Bien Dans Sa Tete" />
+              </a>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
