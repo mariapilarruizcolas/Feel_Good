9581de1560695a8b8246652091709b54a08ff5a6
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2021-11-05 14:47:47 +0100

    Graphic presentation revue

--- src/components/FeelGoodRecipes.jsx
+++ src/components/FeelGoodRecipes.jsx
@@ -1,43 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
 import Recipes from './Recipes';
 import ShowRecipes from './ShowRecipes';
-import Fridge from './Fridge';
-import ShowRecipesFridge from './ShowRecipesFridge';
 import './FeelGoodRecipes.css';
 
 function FeelGoodRecipes() {
-  const [toggleState, setToggleState] = useState(1);
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
   return (
     <div className="container">
-      <div className="bloc-tabs">
-        <button className={toggleState === 1 ? 'tabs active-tabs' : 'tabs'} onClick={() => toggleTab(1)}>
-          Recettes
-        </button>
-        <button className={toggleState === 2 ? 'tabs active-tabs' : 'tabs'} onClick={() => toggleTab(2)}>
-          Frigo
-        </button>
-      </div>
       <div className="content-tabs">
-        <div className={toggleState === 1 ? 'content  active-content' : 'content'}>
-          <div className="FormRecipes">
-            <Recipes />
-          </div>
-          <div>
-            <ShowRecipes />
-          </div>
+        <button className="tabs active-tabs">Recettes</button>
+        <div className="FormRecipes">
+          <Recipes />
         </div>
-
-        <div className={toggleState === 2 ? 'content  active-content' : 'content'}>
-          <div className="FormFridge">
-            <Fridge />
-          </div>
-          <div>
-            <ShowRecipesFridge />
-          </div>
+        <div>
+          <ShowRecipes />
         </div>
       </div>
     </div>
