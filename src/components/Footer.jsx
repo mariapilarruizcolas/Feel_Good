2612c2a7fe1c4bb3c6bc498e99bc4910490b5370
yann_dufaure <yann.dufaure@gmail.com>
2021-11-18 11:05:08 +0100

    create function emails in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import emailjs, { init } from 'emailjs-com';
+
 import './Footer.css';
 
 function Footer() {
+  const [fname, setFname] = useState();
+  const [emailAddress, setEmailAddress] = useState();
+  const [sujet, setSujet] = useState();
+
+  useEffect(() => {
+    init('user_0gLxAobao5UEa4i5G80TE');
+  }, []);
+
+  function handleSendEmail(e) {
+    e.preventDefault();
+    alert(e.target.value);
+
+    const templateParams = {
+      fname: fname,
+      emailAddress: emailAddress,
+      sujet: sujet,
+    };
+    emailjs.send('service_zi12ihe', 'template_wmmlaj9', templateParams);
+  }
+
   return (
     <div className="containerFooter">
       <title>Formulaire de contact</title>
-      <form action="/action_page.php">
+      <form>
         <label htmlFor="fname">Nom & prénom</label>
-        <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom"></input>
+        <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom" onChange={(e) => setFname(e.target.value)}></input>
 
         <label htmlFor="emailAddress">Email</label>
-        <input id="emailAddress" type="email" name="email" placeholder="Votre email"></input>
+        <input id="emailAddress" type="email" name="email" placeholder="Votre email" onChange={(e) => setEmailAddress(e.target.value)}></input>
 
         <label htmlFor="sujet">Message</label>
-        <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message"></input>
+        <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message" onChange={(e) => setSujet(e.target.value)}></input>
 
-        <input type="submit" value="Envoyer"></input>
+        <button className="btnFooter" value="Merci pour votre message!" onClick={handleSendEmail}>
+          Envoyer
+        </button>
       </form>
     </div>
   );
